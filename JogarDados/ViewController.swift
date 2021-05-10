c15c568b73fa06c81abdf7f349092cf93f1c394f
Kauane Gon;alves <kauane.goncalves@hotmail.com>
2021-05-10 17:33:06 -0400

    functionalities

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -9,19 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+    //MARK: Outlet
     @IBOutlet weak var dadoImageView1: UIImageView!
     @IBOutlet weak var dadoImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        dadoImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        
-        dadoImageView1.alpha = 0.5
+                
         
     }
+    //MARK: Actions
+    @IBAction func rolarDadosPressed(_ sender: UIButton) {
+        print("Botao Rolar dados Pressionado!!!")
+        
+        let arrayDados = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
 
-
+        dadoImageView1.image = arrayDados[Int.random(in: 0...5)]
+        dadoImageView2.image = arrayDados[Int.random(in: 0...5)]
+        
+    }
+        
 }
 
